00a49b5b524c79b800b6f00d9809d7c0784f85fa
codeleg <shubhamgupta8609@gmail.com>
2025-08-11 15:11:47 +0530

    Added colors in themes and colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.airbnb.android:lottie:6.6.6")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
